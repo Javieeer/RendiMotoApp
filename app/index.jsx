9e57811d84c9ca0449a161f0f58401c5e5a0c332
Javier Zapata <zapatajavier34@gmail.com>
2025-12-14 17:17:38 -0500

    index funcionando bien

---
+++ app/index.jsx
@@ -0,0 +1,85 @@
+import { router } from 'expo-router';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function Home() {
+
+  const logo = require('../assets/images/rendimoto-logo.png');
+  return (
+    <View style={styles.container}>
+
+      {/* Logo */}
+      <Image source={logo} style={styles.logo} />
+
+      {/* Titulo y subtitulo */}
+      <Text style={styles.title}>
+        RendiMoto App
+      </Text>
+      <Text style={styles.subTitle}>
+        Conoce el verdadero rendimiento de tu vehículo
+      </Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => router.push('/login')}
+      >
+        <Text style={styles.buttonText}>Iniciar sesión</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, styles.secondary]}
+        onPress={() => router.push('/register')}
+      >
+        <Text style={styles.secondaryText}>Registrarse</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F3F3EE',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  logo: {
+    width: 300,
+    height: 150,
+    marginBottom: 20,
+    borderRadius: 20,
+  },
+  title: {
+    color: '#000',
+    fontSize: 28,
+    fontWeight: '700'
+  },
+  subTitle: {
+    color: '#000',
+    fontSize: 16,
+    marginBottom: 20,
+    textAlign: 'center',
+    fontWeight: '500',
+  },
+  button: {
+    width: '100%',
+    backgroundColor: '#FF6B00',
+    padding: 14,
+    borderRadius: 8,
+    marginBottom: 12,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: '600',
+    fontSize: 16,
+  },
+  secondary: {
+    backgroundColor: '#fff',
+  },
+  secondaryText: {
+    color: '#FF6B00',
+    fontWeight: '600',
+    fontSize: 16,
+  },
+});
